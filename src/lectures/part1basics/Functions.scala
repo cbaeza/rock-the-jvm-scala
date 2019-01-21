902772601db32c0bd93a442c28b1ed9d5065df2f
cbaeza <cbaeza@gk-software.com>
2019-01-21 22:29:48 +0100

    add smartFactorial

--- src/lectures/part1basics/Functions.scala
+++ src/lectures/part1basics/Functions.scala
@@ -1,16 +1,20 @@
 package lectures.part1basics
 
+import scala.annotation.tailrec
+
 object Functions extends App {
 
   def aFunction(a: String, b: String): String = {
     a + " " + b
   }
+
   println(aFunction("Hello", "World"))
 
   def repeatAndConcatenate(value: String, times: Int): String = {
     if (times == 1) value
     else value + repeatAndConcatenate(value + "", times - 1)
   }
+
   println(repeatAndConcatenate("Hello", 10))
 
   // 1. Greeting function
@@ -27,6 +31,7 @@ object Functions extends App {
     else
       number * factorial(number - 1)
   }
+
   println("Factorial: " + factorial(5))
 
   // 3. Fibonacci
@@ -34,6 +39,7 @@ object Functions extends App {
     if (n <= 2) 1
     else fibonacci(n - 1) + fibonacci(n - 2)
   }
+
   // 1 1 2 3 5 8 13 21 ...
   println("Fibonacci: " + fibonacci(8))
 
@@ -43,11 +49,24 @@ object Functions extends App {
       if (t <= 1) true
       else n % t != 0 && isPrimeUntil(t - 1)
     }
+
     isPrimeUntil(n / 2)
   }
+
   println("isPrime 2: " + isPrime(2))
   println("isPrime 3: " + isPrime(3))
   println("isPrime 4: " + isPrime(4))
   println("isPrime 5: " + isPrime(5))
   println("isPrime 6: " + isPrime(6))
+
+  // 5. Smart factorial
+  def smartFactorial(n: Int): BigInt = {
+    @tailrec
+    def factorialHelper(x: Int, accumulator: BigInt): BigInt = {
+      if (x <= 1) accumulator
+      else factorialHelper(x - 1, x * accumulator)
+    }
+    factorialHelper(n, 1)
+  }
+  println("smartFactorial " + smartFactorial(120))
 }
